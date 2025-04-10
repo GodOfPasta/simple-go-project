958bcfea8d44ddac488aba8923f733fa4e526448
GodOfPasta <prostoimechko@gmail.com>
2025-04-10 12:08:19 +0800

    routes added

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/", utils.Test)
+	r.POST("/tasks", utils.AddTask)
+	r.GET("/tasks", utils.GetTasks)
+	r.GET("/tasks/:id", utils.GetTask)
+	r.PUT("/tasks/:id", utils.UpdateTask)
+	r.DELETE("/tasks/:id", utils.DeleteTask)
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal("Failed to run server:", err)
--- utils/routes.go
+++ utils/routes.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Test(c *gin.Context) {
+func GetTasks(c *gin.Context) {
 	var tasks []models.Task
 	var dbase, err = db.OpenDBReadConnection()
 
@@ -29,6 +29,93 @@ func Test(c *gin.Context) {
 
 		tasks = append(tasks, task)
 	}
-	c.JSON(http.StatusOK, tasks)
 	db.CloseDBConnection(dbase)
+	c.JSON(http.StatusOK, tasks)
+}
+
+func GetTask(c *gin.Context) {
+	var dbase, err = db.OpenDBReadConnection()
+	id := c.Param("id")
+	var task models.Task
+	err = dbase.QueryRow("SELECT * FROM todo_list.tasks WHERE hash_key = $1", id).
+		Scan(&task.HashKey, &task.Name, &task.Description, &task.Created, &task.Updated, &task.Deadline, &task.Closed)
+
+	if err != nil {
+		if err.Error() == "sql: no rows in result set" {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		} else {
+			panic(err)
+		}
+	}
+
+	db.CloseDBConnection(dbase)
+	c.JSON(http.StatusOK, task)
+}
+
+func AddTask(c *gin.Context) {
+	var task models.Task
+	var dbase, err = db.OpenDBWriteConnection()
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	_, err = dbase.Exec("INSERT INTO todo_list.tasks (name, description, deadline, closed) VALUES ($1, $2, $3, $4)",
+		task.Name, task.Description, task.Deadline, task.Closed)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	err = dbase.QueryRow("SELECT * FROM todo_list.tasks ORDER BY created DESC LIMIT 1").
+		Scan(&task.HashKey, &task.Name, &task.Description, &task.Created, &task.Updated, &task.Deadline, &task.Closed)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.CloseDBConnection(dbase)
+	c.JSON(http.StatusOK, task)
+}
+func UpdateTask(c *gin.Context) {
+	var task models.Task
+	id := c.Param("id")
+	var dbase, err = db.OpenDBWriteConnection()
+
+	if err := c.ShouldBindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	_, err = dbase.Exec("UPDATE todo_list.tasks SET name=$1, description=$2, deadline=$3, closed=$4, updated=now() WHERE hash_key=$5",
+		task.Name, task.Description, task.Deadline, task.Closed, id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+
+	err = dbase.QueryRow("SELECT * FROM todo_list.tasks ORDER BY created DESC LIMIT 1").
+		Scan(&task.HashKey, &task.Name, &task.Description, &task.Created, &task.Updated, &task.Deadline, &task.Closed)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.CloseDBConnection(dbase)
+	c.JSON(http.StatusOK, task)
+}
+func DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	var dbase, err = db.OpenDBWriteConnection()
+	_, err = dbase.Exec("DELETE FROM todo_list.tasks WHERE hash_key=$1", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	db.CloseDBConnection(dbase)
+	c.Status(http.StatusNoContent)
 }
