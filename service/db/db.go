0bc744cbde90df92035e0bae62b3b8ec5e1bed4f
GodOfPasta <prostoimechko@gmail.com>
2025-04-17 13:42:19 +0800

    db added to build

--- service/db/db.go
+++ service/db/db.go
@@ -2,23 +2,20 @@ package db
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
 func OpenDBReadConnection() (*sql.DB, error) {
-	// Hardcode for the testing sake
-	// TODO: get creds from env
-	connStr := "user=db_read password=admin dbname=postgres sslmode=disable"
+	connStr := os.Getenv("DB_READ_URI")
 	db, err := sql.Open("postgres", connStr)
 
 	return db, err
 }
 
 func OpenDBWriteConnection() (*sql.DB, error) {
-	// Hardcode for the testing sake
-	// TODO: get creds from env
-	connStr := "user=db_write password=admin dbname=postgres sslmode=disable"
+	connStr := os.Getenv("DB_WRITE_URI")
 	db, err := sql.Open("postgres", connStr)
 
 	return db, err
